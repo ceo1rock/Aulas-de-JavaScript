50c30bbbeae58ed0dfffe69eac06f06e888526dc
Augusto Rock <augusto.roque89@gmail.com>
2023-12-18 14:33:02 -0300

    update

---
+++ ATIVIDADES/ex009.js
@@ -0,0 +1,12 @@
+var pais = 'EUA'
+if (pais == 'Brasil'){
+    console.log(`Você é Brasileiro`)
+}else{
+    console.log(`Você é Estrangeiro`)
+}
+
+if (pais != 'Brasil'){
+    console.log(`Você é Estrangeiro`)
+}else{
+    console.log(`Você é Brasileiro`)
+}
