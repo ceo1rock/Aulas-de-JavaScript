8ce614fc4dcddf614954898bf88e3ae1d0163373
Augusto Rock <augusto.roque89@gmail.com>
2023-12-20 12:09:08 -0300

    Update
    Alteração do fundo pelo JS

--- ATIVIDADES/AULA 12/ex015 - IDADE E SEXO/scripts.js
+++ ATIVIDADES/AULA 12/ex015 - IDADE E SEXO/scripts.js
@@ -23,27 +23,27 @@ if (fAno.value.length == 0 || Number(fAno.value)>ano)
         if(idade >= 0 && idade < 4) {
             // bebe
             img.setAttribute('src', 'BEBE M.PNG')
-            document.body.style.background = "#0081d8"
+            document.body.style.background = "#eae9f1"
 
         }else if (idade < 12 ){
             // criança
             img.setAttribute('src', 'CRIANCA M.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#c28f74"
 
         }else if ( idade < 17){
             // adolecente
             img.setAttribute('src', 'ADOLESCENTE M.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#773c52"
 
         }else if ( idade < 60){
             // adulto
             img.setAttribute('src', 'ADULTO M.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#d28c56"
 
         }else if ( idade >= 60){
             // idoso
             img.setAttribute('src', 'IDOSO M.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#dbcacf"
 
         }
     }else {
@@ -51,27 +51,27 @@ if (fAno.value.length == 0 || Number(fAno.value)>ano)
         if(idade >= 0 && idade < 4) {
             // bebe
             img.setAttribute('src', 'BEBE F.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#c28f74"
 
         }else if (idade < 12 ){
             // criança
             img.setAttribute('src', 'CRIANCA F.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#e2a95a" 
 
         }else if ( idade < 17){
             // adolecente
             img.setAttribute('src', 'ADOLESCENTE F.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#d4a290"
 
         }else if ( idade < 60){
             // adulto
             img.setAttribute('src', 'ADULTO F.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#d9dceb"
 
         }else if( idade >= 60){
             // idoso
             img.setAttribute('src', 'IDOSO F.PNG')
-            document.body.style.background = 
+            document.body.style.background = "#ddd3d4"
             
         }
     }
